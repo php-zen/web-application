936b7c39bf64f7494ab8ffd70916eda8d0b64f6b
Snakevil Zen <zsnakevil@gmail.com>
2016-07-15 18:06:51 +0800

    提炼 HTTP 请求组件的常用信息将其方法化。

--- lib/IRequest.php
+++ lib/IRequest.php
@@ -3,21 +3,77 @@
  * 声明 Web 应用程序的 HTTP 请求组件规范。
  *
  * @author    Snakevil Zen <zsnakevil@gmail.com>
- * @copyright © 2014 SZen.in
+ * @copyright © 2016 SZen.in
  * @license   LGPL-3.0+
  */
 
 namespace Zen\Web\Application;
 
+use Zen\Core\Type as ZenType;
 use Zen\Core\Application as ZenApp;
 
 /**
  * Web 应用程序的 HTTP 请求组件规范。
  *
- * @package Zen\Web\Application
  * @version 0.1.0
+ *
  * @since   0.1.0
  */
 interface IRequest extends ZenApp\IInput
 {
+    /**
+     * 获取请求协议。
+     *
+     * @return string
+     */
+    public function getProtocol();
+
+    /**
+     * 获取请求主机名称。
+     *
+     * @return string
+     */
+    public function getHost();
+
+    /**
+     * 获取请求目标端口。
+     *
+     * @return int
+     */
+    public function getPort();
+
+    /**
+     * 获取请求资源路径。
+     *
+     * @return string
+     */
+    public function getPath();
+
+    /**
+     * 获取请求查询信息。
+     *
+     * @return string
+     */
+    public function getSearch();
+
+    /**
+     * 获取请求来源。
+     *
+     * @return string
+     */
+    public function getReferer();
+
+    /**
+     * 获取请求源域。
+     *
+     * @return string
+     */
+    public function getOrigin();
+
+    /**
+     * 获取请求时间。
+     *
+     * @return ZenType\DateTime
+     */
+    public function getTime();
 }
--- lib/Request/Request.php
+++ lib/Request/Request.php
@@ -3,24 +3,90 @@
  * 定义 Web 应用程序的 HTTP 请求组件。
  *
  * @author    Snakevil Zen <zsnakevil@gmail.com>
- * @copyright © 2014 SZen.in
+ * @copyright © 2016 SZen.in
  * @license   LGPL-3.0+
  */
 
 namespace Zen\Web\Application\Request;
 
+use Zen\Core\Type as ZenType;
 use Zen\Core\Application as ZenCoreApp;
 use Zen\Web\Application as ZenWebApp;
 
 /**
  * Web 应用程序的 HTTP 请求组件。
  *
- * @package Zen\Web\Application
+ * @property-read string   $protocol
+ * @property-read string   $host
+ * @property-read int      $port
+ * @property-read string   $path
+ * @property-read string   $search
+ * @property-read string   $referer
+ * @property-read string   $origin
+ * @property-read DateTime $time
+ *
  * @version 0.1.0
+ *
  * @since   0.1.0
  */
 class Request extends ZenCoreApp\Input\Input implements ZenWebApp\IRequest
 {
+    /**
+     * 请求协议。
+     *
+     * @var string
+     */
+    protected $protocol;
+
+    /**
+     * 请求主机名称。
+     *
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * 请求目标端口。
+     *
+     * @var int
+     */
+    protected $port;
+
+    /**
+     * 请求资源路径。
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * 请求查询信息。
+     *
+     * @var string
+     */
+    protected $search;
+
+    /**
+     * 请求来源。
+     *
+     * @var string
+     */
+    protected $referer;
+
+    /**
+     * 请求源域。
+     *
+     * @var string
+     */
+    protected $origin;
+
+    /**
+     * 请求时间。
+     *
+     * @var DateTime
+     */
+    protected $time;
+
     /**
      * {@inheritdoc}
      */
@@ -60,7 +126,8 @@ class Request extends ZenCoreApp\Input\Input implements ZenWebApp\IRequest
     /**
      * {@inheritdoc}
      *
-     * @param  string $abbr 命名空间缩写
+     * @param string $abbr 命名空间缩写
+     *
      * @return string
      */
     protected function guessAbbr($abbr)
@@ -86,7 +153,8 @@ class Request extends ZenCoreApp\Input\Input implements ZenWebApp\IRequest
     /**
      * 对象化上传文件信息。
      *
-     * @param  array                 $value 上传文件信息
+     * @param array $value 上传文件信息
+     *
      * @return File\File|File\File[]
      */
     protected function onGetFile($value)
@@ -112,5 +180,120 @@ class Request extends ZenCoreApp\Input\Input implements ZenWebApp\IRequest
     public function summarize()
     {
         return $this->params['server']['PATH_INFO'];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getProtocol()
+    {
+        if (!$this->protocol) {
+            $this->protocol = strtolower(preg_replace('#/.*$#', '', $this->params['server']['SERVER_PROTOCOL'])).':';
+        }
+
+        return $this->protocol;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getHost()
+    {
+        if (!$this->host) {
+            $this->host = $this->params['server']['HTTP_HOST'];
+        }
+
+        return $this->host;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return int
+     */
+    public function getPort()
+    {
+        if (!$this->port) {
+            $this->port = (int) $this->params['server']['SERVER_PORT'];
+        }
+
+        return $this->port;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        if (!$this->path) {
+            $this->path = preg_replace('#\?.*$#', '', $this->params['server']['REQUEST_URI']);
+        }
+
+        return $this->path;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getSearch()
+    {
+        if (!is_string($this->search)) {
+            $this->search = '?'.$this->params['server']['QUERY_STRING'];
+            if ('?' == $this->search) {
+                $this->search = '';
+            }
+        }
+
+        return $this->search;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getReferer()
+    {
+        if (!is_string($this->referer)) {
+            $this->referer = (string) $this->params['server']['HTTP_REFERER'];
+        }
+
+        return $this->referer;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getOrigin()
+    {
+        if (!is_string($this->origin)) {
+            $this->origin = (string) $this->params['server']['HTTP_ORIGIN'];
+        }
+
+        return $this->origin;
+    }
+
+    /**
+     * 获取请求时间。
+     *
+     * @return ZenType\DateTime
+     */
+    public function getTime()
+    {
+        if (!$this->time) {
+            $this->time = new ZenType\DateTime($this->params['server']['REQUEST_TIME']);
+        }
+
+        return $this->time;
     }
 }
